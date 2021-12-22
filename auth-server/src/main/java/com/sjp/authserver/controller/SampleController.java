69c33c4d5ef60dfb66486eb5bcc719e134550474
puneeth-sj-git <puneeth9218@gmail.com>
2021-12-22 09:31:48 +0530

    foo login

--- auth-server/src/main/java/com/sjp/authserver/controller/SampleController.java
+++ auth-server/src/main/java/com/sjp/authserver/controller/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 	 
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	public String m1() {
 		return "Hello World";
 	}
