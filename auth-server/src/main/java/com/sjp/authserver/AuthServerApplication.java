92d1bfe289fea42f489eb420aa2173b20c66a90a
puneeth-sj-git <puneeth9218@gmail.com>
2021-12-23 17:24:06 +0530

    aut implemented with default user

--- auth-server/src/main/java/com/sjp/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/sjp/authserver/AuthServerApplication.java
@@ -3,6 +3,7 @@ package com.sjp.authserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AuthServerApplication {
 
--- auth-server/src/main/java/com/sjp/authserver/controller/SampleController.java
+++ auth-server/src/main/java/com/sjp/authserver/controller/SampleController.java
@@ -1,13 +1,54 @@
 package com.sjp.authserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sjp.authserver.model.AuthenticationRequest;
+import com.sjp.authserver.model.AuthenticationResponse;
+import com.sjp.authserver.service.MyUserDetailsService;
+import com.sjp.authserver.util.JwtUtil;
+
 @RestController
+@RequestMapping("/")
 public class SampleController {
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	MyUserDetailsService myUserDetailsService;
+	
+	@Autowired
+	JwtUtil jwtUtil;
 	 
-	@RequestMapping("/")
+	@RequestMapping("hello")
 	public String m1() {
 		return "Hello World";
+	}
+	
+	@RequestMapping(value="authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthnticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+					);
+		}catch(BadCredentialsException e) {
+			throw new Exception("Incorrect username or password!", e);
+		}
+		final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		final String jwt = jwtUtil.generateToken(userDetails);
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+		//return new ResponseEntity<AuthenticationResponse>(new AuthenticationResponse(jwt), HttpStatus.OK);
 	}
 }
